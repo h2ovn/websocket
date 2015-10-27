ae26f9731b18e6529ac6a9c20050b95486858db4
h2ovn <h2o_vn@hotmail.com>
2015-10-27 16:22:13 +0700

    đơn giản

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-var express = require("express");
-var app = express();
-var port = 8888;
- 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
-    res.render("page");
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var clients = [];
+
+var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets server
+    // we don't have to implement anything.
+});
+server.listen(1337, function() {
+  console.log((new Date()) + " Server is listening on port 1337");
 });
 
-app.use(express.static(__dirname + '/public')); 
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
 
+function sendCallback(err) {
+    if (err) console.error("send() error: " + err);
+}
 
-console.log("Listening on port " + port);
-
+// This callback function is called every time someone
+// tries to connect to the WebSocket server
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+    var connection = request.accept(null, request.origin);
+    console.log(' Connection ' + connection.remoteAddress);
+    clients.push(connection);
+    
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+            console.log((new Date()) + ' Received Message ' + message.utf8Data);
+            // broadcast message to all connected clients
+            clients.forEach(function (outputConnection) {
+                if (outputConnection != connection) {
+                  outputConnection.send(message.utf8Data, sendCallback);
+                }
+            });
+        }
+    });
+    
+    connection.on('close', function(connection) {
+        // close user connection
+        console.log((new Date()) + " Peer disconnected.");        
+    });
+});
